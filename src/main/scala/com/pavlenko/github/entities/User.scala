d5de2da222bf0b3e62ce85855c4bf3e8ca085937
Sergii Pavlenko <sm.pavlenko@gmail.com>
2017-11-06 22:13:01 +0100

    added rest call, parsed result

--- src/main/scala/com/pavlenko/github/entities/User.scala
+++ src/main/scala/com/pavlenko/github/entities/User.scala
@@ -1,8 +1,19 @@
 package com.pavlenko.github.entities
 
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
 /**
  * @author sergii.pavlenko
  * @since Nov 5, 2017
  */
 case class User(id: String, name: String) {
+}
+
+object User {
+  implicit val userReads: Reads[User] = (
+    (JsPath \ "login").read[String] and
+      (JsPath \ "url").read[String]
+    )(User.apply _)
 }
--- src/main/scala/com/pavlenko/github/services/UserService.scala
+++ src/main/scala/com/pavlenko/github/services/UserService.scala
@@ -1,15 +1,23 @@
 package com.pavlenko.github.services
 
 import com.pavlenko.github.entities.User
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
 import scala.concurrent.{Future, ExecutionContext}
+import scala.io.Source
 
 /**
  * @author sergii.pavlenko
  * @since Nov 5, 2017
  */
 class UserService(implicit val executionContext: ExecutionContext) {
-
-  def getUser(id: String): Future[Option[User]] = Future {
-    Some(User(id, "username"))
+  def getUser(id: String): Future[List[User]] = Future {
+    val url = "https://api.github.com/search/users?q=language:"
+    val result = Source.fromURL(url + id).mkString
+    val parsed = Json.parse(result)
+    val peoples = (parsed \ "items").validate[List[User]].get
+    peoples
   }
+
 }
