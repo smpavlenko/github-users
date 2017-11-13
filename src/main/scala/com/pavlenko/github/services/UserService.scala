bdfc3571afc8fc45b184c700f46d2fc681ea87ad
Sergii Pavlenko <sm.pavlenko@gmail.com>
2017-11-13 19:41:02 +0100

    Added error handling in case of wrong language

--- src/main/scala/com/pavlenko/github/services/UserService.scala
+++ src/main/scala/com/pavlenko/github/services/UserService.scala
@@ -6,6 +6,7 @@ import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 import scala.concurrent.{Future, ExecutionContext}
 import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
 /**
  * @author sergii.pavlenko
@@ -14,24 +15,28 @@ import scala.io.Source
 class UserService(implicit val executionContext: ExecutionContext) {
 
   implicit val personReader: Reads[(String, String)] = (
-      (__ \ "login").read[String] and
+    (__ \ "login").read[String] and
       (__ \ "avatar_url").read[String]
     ).tupled
 
   def getUser(id: String): Future[List[User]] = Future {
-    val users = Source.fromURL(s"https://api.github.com/search/users?q=language:$id&per_page=5").mkString
-    val parsedUsers = Json.parse(users)
-    val people = (parsedUsers \ "items").as[List[(String, String)]]
+    val json = Try(Source.fromURL(s"https://api.github.com/search/users?q=language:$id&per_page=5").mkString)
+    json match {
+      case Success(users) =>
+        val parsedUsers = Json.parse(users)
+        val people = (parsedUsers \ "items").as[List[(String, String)]]
+        val res = people
+          .map(_._1)
+          .map(login => Source.fromURL(s"https://api.github.com/users/$login"))
+          .map(_.mkString)
+          .map(Json.parse(_))
+          .map(_.validate[User])
+          .map(_.get)
 
-    val res = people
-      .map(_._1)
-      .map(login => Source.fromURL(s"https://api.github.com/users/$login"))
-      .map(_.mkString)
-      .map(Json.parse(_))
-      .map(_.validate[User])
-      .map(_.get)
-
-    res
+        res
+      case Failure(e) =>
+        List()
+    }
   }
 
 }
